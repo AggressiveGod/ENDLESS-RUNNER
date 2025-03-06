c0bbaa79ec913690b82ad8f4fcb79ee10f9afdb9
AggressiveGod <gsmingismylife@gmail.com>
2025-03-06 02:24:44 +0100

    Day 2 and 3

--- Assets/Scripts/Player Movment.cs
+++ Assets/Scripts/Player Movment.cs
@@ -2,35 +2,30 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMovment : MonoBehaviour
+public class PlayerMovement : MonoBehaviour
 {
-    public float playerSpeed = 6;
-    public float horizontalSpeed = 3;
-    public float Rightlimit = 5.5f;
-    public float Leftlimit = -5.5f;
-
+    public float playerSpeed = 10;
+    public float horizontalSpeed = 5;
+    public float rightLimit = 5.5f;
+    public float leftLimit = -5.5f;
 
     void Update()
     {
-
-
         transform.Translate(Vector3.forward * Time.deltaTime * playerSpeed, Space.World);
         if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
         {
-            if (this.gameObject.transform.position.x > Leftlimit)
+            if (this.gameObject.transform.position.x > leftLimit)
             {
                 transform.Translate(Vector3.left * Time.deltaTime * horizontalSpeed);
             }
         }
         if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
         {
-                if (this.gameObject.transform.position.x < Rightlimit)
-                {
-                    transform.Translate(Vector3.left * Time.deltaTime * horizontalSpeed * -1);
-                }
-            
+            if (this.gameObject.transform.position.x < rightLimit)
+            {
+                transform.Translate(Vector3.left * Time.deltaTime * horizontalSpeed * -1);
+            }
         }
     }
 }
-
 
